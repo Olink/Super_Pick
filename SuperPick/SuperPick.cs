34fc37f2efae82c4a8b83b6bf43ec6c532beb374
Ijwu <ijwu@hotmail.com>
2012-07-14 23:59:40 -0400

    Added recursive pick.

--- SuperPick/SuperPick.cs
+++ SuperPick/SuperPick.cs
@@ -11,7 +11,17 @@ namespace SuperPick
     [APIVersion( 1,12)]
     public class SuperPick : TerrariaPlugin
     {
-        private bool[] players;
+        public struct player
+        {
+            public player(bool toggle, bool recurve)
+            {
+                this.toggle = toggle;
+                this.recurve = recurve;
+            }
+            public bool toggle;
+            public bool recurve;
+        }
+        public player[] players;
 
         public override string Author
         {
@@ -36,10 +46,11 @@ namespace SuperPick
         public SuperPick(Main game)
             : base(game)
         {
-            players = new bool[byte.MaxValue];
+            players = new player[255];
+            player normal = new player(false, false);
             for( int i = 0; i < players.Length; i++ )
             {
-                players[i] = false;
+                players[i] = normal;
             }
         }
 
@@ -48,18 +59,20 @@ namespace SuperPick
             Hooks.ServerHooks.Leave += OnLeave;
             GetDataHandlers.TileEdit += OnTileEdit;
             Commands.ChatCommands.Add( new Command("superpick", Toggle, "superpick", "sp"));
+            Commands.ChatCommands.Add(new Command("recursivepick", RecurveToggle, "recursive"));
         }
 
         private void OnLeave( int who )
         {
-            players[who] = false;
+            players[who].toggle = false;
+            players[who].recurve = false;
         }
 
         private void OnTileEdit( object sender, GetDataHandlers.TileEditEventArgs args )
         {
-            if (players[args.Player.Index])
+            if (players[args.Player.Index].toggle)
             {
-                switch( args.EditType )
+                switch (args.EditType)
                 {
                     case 0:
                     case 4:
@@ -76,13 +89,63 @@ namespace SuperPick
                         break;
                 }
             }
+            else
+            {
+                if (players[args.Player.Index].recurve)
+                {
+                    switch (args.EditType)
+                    {
+                        case 0:
+                        case 4:
+                            {
+                                Recursive re = new Recursive();
+                                List<Vector2> del = re.RecursiveEdit(args.X, args.Y, args.EditType);
+                                foreach (Vector2 tip in del)
+                                {
+                                    WorldGen.KillTile(Convert.ToInt16(tip.X), Convert.ToInt16(tip.Y));
+                                    TSPlayer.All.SendTileSquare(Convert.ToInt16(tip.X), Convert.ToInt16(tip.Y), 1);
+                                }
+                                break;
+                            }
+                        case 2:
+                            {
+                                Recursive re = new Recursive();
+                                List<Vector2> del = re.RecursiveEdit(args.X, args.Y, args.EditType);
+                                foreach (Vector2 tip in del)
+                                {
+                                    WorldGen.KillWall(Convert.ToInt16(tip.X), Convert.ToInt16(tip.Y));
+                                    TSPlayer.All.SendTileSquare(Convert.ToInt16(tip.X), Convert.ToInt16(tip.Y), 1);
+                                }
+                                break;
+                            }
+                        case 6:
+                            {
+                                Recursive re = new Recursive();
+                                List<Vector2> del = re.RecursiveEdit(args.X, args.Y, args.EditType);
+                                foreach (Vector2 tip in del)
+                                {
+                                    WorldGen.KillWire(Convert.ToInt16(tip.X), Convert.ToInt16(tip.Y));
+                                    TSPlayer.All.SendTileSquare(Convert.ToInt16(tip.X), Convert.ToInt16(tip.Y), 1);
+                                }
+                                break;
+                            }
+                    }
+                }
+            }
         }
 
         private void Toggle( CommandArgs args )
         {
-            players[args.Player.Index] = (!players[args.Player.Index]);
-            args.Player.SendMessage(String.Format("You have {0} superpick.", (players[args.Player.Index] ? "enabled" : "disabled")), Color.Green);
+            players[args.Player.Index].toggle = (!players[args.Player.Index].toggle);
+            args.Player.SendMessage(String.Format("You have {0} superpick.", (players[args.Player.Index].toggle ? "enabled" : "disabled")), Color.Green);
         }
+
+        private void RecurveToggle(CommandArgs args)
+        {
+            players[args.Player.Index].recurve = (!players[args.Player.Index].recurve);
+            args.Player.SendMessage(String.Format("You have {0} recursivepick.", (players[args.Player.Index].recurve ? "enabled" : "disabled")), Color.Green);
+        }
+       
         protected override void Dispose(bool disposing)
         {
             if (disposing)
